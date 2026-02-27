5df21c6b6f08cab5181bbe7fcce18babccd2acf5
evetechnologysolution <dev@evetechsolution.com>
2026-02-27 16:42:39 +0700

    update random id

--- src/utils/generateRandom.js
+++ src/utils/generateRandom.js
@@ -1,10 +1,12 @@
 export const generateRandomId = (limitChar = 10) => {
-    const randomNum = Math.floor(Math.random() * Date.now()) + Math.floor(Math.random() * 1000000);
+  const randomNum = Math.floor(Math.random() * 1000000) + Math.floor(performance.now());
 
-    // Mengubah angka menjadi string dan mengambil limit karakter pertama
-    const id = randomNum.toString().slice(0, limitChar);
+  // Mengubah angka menjadi string dan mengambil limit karakter pertama
+  const id = randomNum.toString().slice(0, limitChar);
 
-    const currYear = new Date().getFullYear();
+  const currYear = new Date().getFullYear().toString().slice(-2); // 2 digit tahun
+  const currMonth = String(new Date().getMonth() + 1).padStart(2, '0'); // 2 digit bulan
+  const currDate = String(new Date().getDate()).padStart(2, '0'); // 2 digit tanggal
 
-    return `ORD${currYear}${id}`;
-}
+  return `OR${currYear}${currMonth}${currDate}${id}`;
+};
